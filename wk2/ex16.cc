1284cda0cdf9441774854a66b924a49e437ccd69
B.J. Boland <s2591359@fwn-bborg-edu-74-4.fmnsedu.rug.nl>
2019-09-23 14:42:58 +0200

    Can't use enums need to use array (can we?)

--- wk2/ex16.cc
+++ wk2/ex16.cc
@@ -9,19 +9,20 @@ using namespace std;
 int main(int argc, char *argv[])
 {
   // To map letters to their corresponding (decimal) integral value we use an enum
-  enum Lnums { a = 10, b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z };
-  Lnums value = z;
-  std::cout << z;
-  
-  size_t input = argv[1];
-  size_t radix = argv[2];
+  char digits[] = {'0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+   
+  /*
+  size_t radix = stoi(argv[1]);
+  size_t input = stoi(argv[2]);
+  */
+  size_t radix = 5;
+  size_t input = 34; 
   string output;
 
   // Probably better to use a for loop, this is only temporary
   while(true)
   {
-    // NOTE :: Can we use integer division like this? We want it to throw away the remainder
-    // I think this works but test this
+    break;
     if(input / radix != 0)
     {
       // Calculate the remainder of this division, that is our digit
@@ -30,6 +31,9 @@ int main(int argc, char *argv[])
       // this digit is more significant than any that went before
       // so we are pushing the numbers to the right
       // we have at least one more iteration to go
+      size_t remainder = input % radix;
+      char digit = digits[remainder];
+      
     } else
     {
       // this means we have reached the end of our divisions and we need to add 
