5eeef62b76a7e0ebf60ccd270abab4772a83b824
Bastiaan Boland <bastiaan.boland@gmail.com>
2019-09-23 12:08:50 +0200

    Finished ex14, it works!!

--- wk2/ex14.cc
+++ wk2/ex14.cc
@@ -22,30 +22,26 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-  // The number of combinations is 2^(argc-1), here calculated by using bitshift
-  cout << "Calc numcomb" << endl;
-  int numcomb = 1 << (argc - 1);
-  cout << numcomb << endl;
-
-  // We now count up from 0 to the total number of combinations 
-  for(int count = 0; count < numcomb; ++count)
+  // To show all combinations of arguments we can use binary counting.
+  size_t numbits = argc - 1; // As the first argument is the program name we have 1 less argument to show
+  size_t numcomb = 1 << numbits; // This is the same as 2^(numbits)
+  
+  for(size_t count = 0; count < numcomb; ++count) // Each different combination has a unique countnumber
   {
     string output;
 
-    // We are now calculating which bits are on and which are off.
-    // Using this we know which args to include. 
-    for(size_t bitscounter = 0, bits = count; bits > 0; ++bitscounter, bits >> 1)
+    // We now loop over every 'place' in the binary number, with each iteration we
+    // bitshift countcpy to the right thereby getting a new least significant bit (LSB) 
+    for(size_t place = 1, countcpy = count; place <= numbits; ++place, countcpy >>= 1)
     {
-      if(bits % 2 == 1)
+      if(countcpy % 2 == 1) // Check the value of the LSB, if it is 1 then add the arg to the output
       {
-        // Add arg to output
-        output += argv[bitscounter]; 
-        output += ' ';
-        cout << "added: " << output << endl;
+        output += argv[place];
+        output += " ";
       } 
     }
 
-    // Output the line
-    cout << count << ": " << output << '\n';
+    cout << count << ": " << output << '\n'; // Show a combination
   }
+
 }
