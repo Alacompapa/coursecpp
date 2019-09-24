8de767f25d386b0d2965cae116d64f36b917498f
B.J. Boland <s2591359@fwn-bborg-edu-74-46.fmnsedu.rug.nl>
2019-09-24 13:59:00 +0200

    Indentation was wrong

--- wk2/charlisting.cc
+++ wk2/charlisting.cc
@@ -4,15 +4,15 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-  // We need a string to store the letters we find
-  string letterset;
+    // We need a string to store the letters we find
+    string letterset;
 
-  // A char has size of one byte which can be used as a number or as a character
-  for (unsigned char asciicode = 0; asciicode < 255; ++asciicode) 
-    // Check if the byte is a letter in the ASCII encoding and add it to the set
-    if (isalpha(asciicode))
-      letterset += asciicode;
-   
-  // Output the set to the cout stream
-  cout << letterset << '\n';
+    // A char has size of one byte which can be used as a number 
+    for (unsigned char asciicode = 0; asciicode < 255; ++asciicode) 
+        // Check if the byte is a letter in the ASCII encoding and add it
+        if (isalpha(asciicode))
+            letterset += asciicode;
+
+    // Output the set to the cout stream
+    cout << letterset << '\n';
 }
