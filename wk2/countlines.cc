57a715fd3205a76c40152211262b5efb17b53acc
j-boers-13 <j.boers.13@student.rug.nl>
2019-09-24 14:45:56 +0200

    fixed v2

--- wk2/countlines.cc
+++ wk2/countlines.cc
@@ -4,30 +4,30 @@ using namespace std; // Use the std namespace
 
 int main(int argc, char *argv[])
 {
-  string argument = (argc > 1 ? argv[1]:);
-  // Check if any argument is given, and if that argument is ok
-  if (argument == "ok")
-  {
-    // Count the number of lines
-    int count = 0;
-    string line;
-    while (getline(cin, line))
-      ++count;
-    cout << count << '\n';
-  }
-  else
-  {
-    // No argument given, or argument is not "ok"
-    // Here we count the lines in the wrong way
-    // (and getting a result 1 greater than expected)
-    int count = 0;
-    string line;
-    while (true)
+    string argument = (argc > 1 ? argv[1]:"");
+    // Check if any argument is given, and if that argument is ok
+    if (argument == "ok")
     {
-      ++count;
-      if (not getline(cin,line))
-        break;
+        // Count the number of lines
+        int count = 0;
+        string line;
+        while (getline(cin, line))
+            ++count;
+        cout << count << '\n';
     }
-    cout << count << '\n';
-  }
+    else
+    {
+        // No argument given, or argument is not "ok"
+        // Here we count the lines in the wrong way
+        // (and getting a result 1 greater than expected)
+        int count = 0;
+        string line;
+        while (true)
+        {
+            ++count;
+            if (not getline(cin,line))
+                break;
+        }
+        cout << count << '\n';
+    }
 }
