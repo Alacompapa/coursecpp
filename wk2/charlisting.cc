9cf65b96187b263072490f709913630c79b1f123
B.J. Boland <s2591359@fwn-bborg-edu-74-46.fmnsedu.rug.nl>
2019-09-24 13:49:26 +0200

    Corrected ex10

--- wk2/charlisting.cc
+++ wk2/charlisting.cc
@@ -1,6 +1,6 @@
 #include <iostream>
 
-using namespace std; // Use the std namespace
+using namespace std; 
 
 int main(int argc, char *argv[])
 {
@@ -8,16 +8,11 @@ int main(int argc, char *argv[])
   string letterset;
 
   // A char has size of one byte which can be used as a number or as a character
-  for (char asciicode = 1; asciicode < 127; asciicode++) 
-  {
+  for (unsigned char asciicode = 0; asciicode < 255; ++asciicode) 
     // Check if the byte is a letter in the ASCII encoding and add it to the set
-    if (isalpha(asciicode)){
+    if (isalpha(asciicode))
       letterset += asciicode;
-    }
-  }
    
   // Output the set to the cout stream
-  cout << letterset << endl;
-
-  return 0;
+  cout << letterset << '\n';
 }
