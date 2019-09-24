866a8b00e40758449b2dc3b3d5dcf564178dc27d
j-boers-13 <j.boers.13@student.rug.nl>
2019-09-24 14:07:37 +0200

    Tried to fix 11

--- README.md
+++ README.md
@@ -6,6 +6,7 @@
   - So I already found out the way I'm adding comments now is wrong
   - We should set a hard limit at linelength 80 (so we never have any printer issues)
 - Which build tools are we going to use? I thought `icmake` looked nicest
+  icmstart icmbuild - these two are important i think.
 - I'm not a master git user so I don't know how to add you so you can also accept pull requests
 
 
--- wk2/countlines.cc
+++ wk2/countlines.cc
@@ -4,37 +4,29 @@ using namespace std; // Use the std namespace
 
 int main(int argc, char *argv[])
 {
-  int count = 0;
-  string line;
-
-  // Check if any argument is given
-  if(argc > 1)
+  string argument = argv[1];
+  // Check if any argument is given, and if that argument is ok
+  if((argc > 1) and (argument == "ok"))
   {
-    // Check if argument "ok" is given
-    string argument = argv[1];
-    if(argument == "ok")
-    {
-      // Count the number of lines
-      while(getline(cin, line))
-      {
-        ++count;
-      }
-      cout << count << endl;
-
-      return 0;
-    }
-  }
-
-  // No argument given, or argument is not "ok"
-  // Here we count the lines in the wrong way 
-  // (and getting a result 1 greater than expected)
-  while(true){
+    // Count the number of lines
+    int count = 0;
+    string line;
+    while(getline(cin, line))
       ++count;
-    if (not getline(cin,line)){
-      break;
-    }
+    cout << count << '\n';
   }
-  cout << count << endl;
-
-  return 0;
+  else{
+    // No argument given, or argument is not "ok"
+    // Here we count the lines in the wrong way
+    // (and getting a result 1 greater than expected)
+    int count = 0;
+    string line;
+    while(true)
+    {
+      ++count;
+      if (not getline(cin,line))
+        break;
+    }
+    cout << count << '\n';
+  }
 }
