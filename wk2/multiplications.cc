df091a91604d6ed6a856116f6e46d26099713ac7
j-boers-13 <j.boers.13@student.rug.nl>
2019-09-24 15:03:24 +0200

    Implemented stoi

--- wk2/multiplications.cc
+++ wk2/multiplications.cc
@@ -4,12 +4,10 @@ using namespace std; // Use the std namespace
 
 int main(int argc, char *argv[])
 {
-    int input = static_cast<int>(argv[1]); // To use the argument as a number we need to do this
+    int input = stoi(argv[1]); // To use the argument as a number we need to do this
 
     // We want to create a multiplication table up to and including 10
     for (size_t count = 1; count != 11 ; ++count)
-    {
         // Output contents to cout stream
         cout << count << " * " << input << " = " << count * input << '\n';
-    }
 }
