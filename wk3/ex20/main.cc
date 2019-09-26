e8240f7861d1d54704196c7770ed99dd2965cd7b
Bastiaan Boland <bastiaan.boland@gmail.com>
2019-09-26 22:14:56 +0200

    Ex20

--- wk3/ex20/main.cc
+++ wk3/ex20/main.cc
@@ -1,8 +1,14 @@
 #include "main.ih"
 
-int main(int argc, char **argv)
+int main(int argc, char **argv, char **envp)
 try
 {
+    // test code for printing all environment variables
+    for (char **env = envp; *env != 0; env++)
+    {
+        char *thisEnv = *env;
+        cout << thisEnv << '\n';
+    }
 }
 catch (...)
 {
--- wk3/ex20/quicksort.cc
+++ wk3/ex20/quicksort.cc
@@ -1,5 +1,7 @@
 #include "main.ih"
 
-void quicksort(string &toSort, size_t left, size_t right)
+void quicksort(string toSort[]) 
 {
+    // look @ the Annos to find what logical operators are defined for 
+    // strings (like >= or < or ==)
 }
