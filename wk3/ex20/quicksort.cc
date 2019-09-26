67f074e7e6941df6c7b8a59fc7a78339b11cf03c
Bastiaan Boland <bastiaan.boland@gmail.com>
2019-09-26 22:17:33 +0200

    quicksort needs to have left and right params

--- wk3/ex20/quicksort.cc
+++ wk3/ex20/quicksort.cc
@@ -1,6 +1,6 @@
 #include "main.ih"
 
-void quicksort(string toSort[]) 
+void quicksort(string toSort[], size_t left, size_t right) 
 {
     // look @ the Annos to find what logical operators are defined for 
     // strings (like >= or < or ==)
