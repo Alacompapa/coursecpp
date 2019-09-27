70be9656550d4db84ba2f675946a6ed7ae262f0e
j-boers-13 <j.boers.13@student.rug.nl>
2019-09-27 13:23:58 +0200

    Some try at 20, but really not much at all

--- wk3/ex20/quickSort.cc
+++ wk3/ex20/quickSort.cc
@@ -2,7 +2,14 @@
 
 void quickSort()
 {
-    for (size_t i = 0; environ[i]; ++i) {
+    size_t i;
+    for (i = 0; environ[i]; ++i)
+    {
+
         cout << environ[i] << '\n';
     }
+    size_t first_pos = 0;
+    size_t last_pos = i;
+    size_t pivot_pos = partition(environ, first_pos, last_pos)
+
 }
