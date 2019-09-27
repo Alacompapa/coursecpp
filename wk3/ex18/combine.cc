f31d279f703e9a95aa53970e989b3c34c282f59c
B.J. Boland <s2591359@fwn-bborg-edu-74-8.fmnsedu.rug.nl>
2019-09-27 14:50:43 +0200

    Added comments ex18

--- wk3/ex18/combine.cc
+++ wk3/ex18/combine.cc
@@ -2,15 +2,15 @@
 
 ReturnValues combine(int argc, char **argv)
 {
-    ReturnValues retVal;
+    ReturnValues retVal;                            // struct to be returned
 
-    if (argc == 2 or stoi(argv[1]) >= argc) 
-        retVal.ok = false;
+    if (argc == 2 or stoi(argv[1]) >= argc)         // only true if requested
+        retVal.ok = false;                          // arg exists
     else
         retVal.ok = true;
 
-    retVal.nr = stoi(argv[1]);
-
+    retVal.nr = stoi(argv[1]);                      // requested arg number
+                                                    // requested arg value
     retVal.value = retVal.ok ? argv[retVal.nr] : "";
 
     return retVal;
