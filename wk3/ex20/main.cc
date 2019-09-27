09cbe33f0a08cc54c02f24b7d690826de1d6075b
j-boers-13 <j.boers.13@student.rug.nl>
2019-09-27 13:14:58 +0200

    Fixed quicksort base setup

--- wk3/ex20/main.cc
+++ wk3/ex20/main.cc
@@ -2,10 +2,5 @@
 
 int main(int argc, char **argv)
 {
-    size_t left = 0;
-    size_t right = 0;
-    quicksort(environ, left, right);
-    for (size_t i = 0; environ[i]; ++i) {
-        cout << environ[i] << '\n';
-    }
+    quickSort();
 }
---
+++ wk3/ex20/quickSort.cc
@@ -0,0 +1,8 @@
+#include "main.ih"
+
+void quickSort()
+{
+    for (size_t i = 0; environ[i]; ++i) {
+        cout << environ[i] << '\n';
+    }
+}
