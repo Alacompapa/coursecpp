be045b6740a9cd09c805ad1fed970a2a0fb92525
Alacompapa <jashiel.work98@gmail.com>
2019-09-30 19:16:27 +0200

    ex27

--- wk4/ex27/person.h
+++ wk4/ex27/person.h
@@ -1,11 +1,24 @@
 #ifndef PERSON_H
 #define PERSON_H
-
+#include <iostream>
+#include <string>
 
 class Person
 {
+  std::string d_name;
+  std::string d_address;
+  std::string d_phone;
+  size_t      d_mass;
 public:
-    Person();
+    void setName(std::string const &name);
+    void setAddress(std::string const &address);
+    void setPhone(std::string const &phone);
+    void setMass(size_t mass);
+
+    std::string const &name()     const;
+    std::string const &address()  const;
+    std::string const &phone()    const;
+    size_t mass()                 const;
 };
 
 #endif // PERSON_H
