9ee358a8128dbbc8eb63e2c878c1d2fc417f7298
Bastiaan Boland <bastiaan.boland@gmail.com>
2019-10-01 13:03:08 +0200

    Very small changes

--- wk3/ex22/show.cc
+++ wk3/ex22/show.cc
@@ -3,7 +3,7 @@
 void show(bool bits[], size_t size)          
 {                                       
     for (size_t idx = 0; idx != size; ++idx) 
-        if (bits[idx] == true)
+        if (bits[idx])
             cout << idx << ' ';
 
     cout << '\n';                       // Newline only at end of display            
