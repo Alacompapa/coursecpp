f2f69e221ca36813086a36dc5214f733878d7100
j-boers-13 <j.boers.13@student.rug.nl>
2019-10-01 23:05:03 +0200

    ex17 fixed

--- wk3/ex17/double.cc
+++ wk3/ex17/double.cc
@@ -1,9 +1,9 @@
 #include "main.ih"
 
-double sum(double argc, char *argv[])
+double sum(int argc, char *argv[], size_t counter)
 {
     double summed = 0;                         // Loop over amount of args
-    for (size_t idx = 1; idx != static_cast<size_t>(argc); ++idx)  
+    for (size_t idx = 1; idx != static_cast<size_t>(argc); ++idx)
         summed += stod(argv[idx]);             // Add double to sum
 
     return summed;
