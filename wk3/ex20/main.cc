f6b7318a392fa992e898f44d1dbc008089a400d3
Bastiaan Boland <bastiaan.boland@gmail.com>
2019-10-01 15:39:20 +0200

    no message

--- wk3/ex20/main.cc
+++ wk3/ex20/main.cc
@@ -5,7 +5,11 @@ int main(int argc, char **argv)
     for (size_t idx = 1; environ[idx]; ++idx)
         cout << environ[idx] << '\n';
 
+   
+
+/*
     char** env[] = { environ, nullptr };
     for (size_t idx = 1; env[idx]; ++idx)
         cout << env[idx] << '\n';
+*/
 }
--- wk3/ex20/partition.cc
+++ wk3/ex20/partition.cc
@@ -1,5 +1,22 @@
 #include "main.ih"
 
-void partition(string array[], size_t left, size_t right)
+size_t partition(string array[], size_t left, size_t right)
 {
+    string pivot = array[right];
+
+    for (size_t idx = left; idx < right; ++idx)
+    {
+        if (array[idx] <= pivot)
+        {
+            string tmp = array[idx];
+            array[idx] = array[idx + 1];
+            array[idx + 1] = tmp;
+        }
+    }
+
+    string tmp = array[left];
+    array[left] = array[right];
+    array[right] = array[left];
+
+    return left;
 }
