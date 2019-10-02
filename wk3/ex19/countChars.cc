fc90dd62843873890cfd02ed360f21116764ee1e
Bastiaan Boland <bastiaan.boland@gmail.com>
2019-10-02 10:24:13 +0200

    Only need to do countWords now

--- wk3/ex19/countChars.cc
+++ wk3/ex19/countChars.cc
@@ -5,9 +5,7 @@ void countChars()
     size_t count = 0;
     string line;
     while (getline(cin, line))
-        for (size_t idx = 0; idx != line.length(); ++idx)
-            if (isalnum(line[idx]))             // all alphanumeric chars are 
-               ++count;                         // counted
+        count += line.length();
 
     cout << count << " characters in stream.\n";
 }
