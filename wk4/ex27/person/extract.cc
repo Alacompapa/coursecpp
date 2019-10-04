b7bd472111209f58254bfb2e4c3e13c84c3d2e05
Bastiaan Boland <bastiaan.boland@gmail.com>
2019-10-04 12:27:00 +0200

    enums dont have arithmetic operators it seems

--- wk4/ex27/person/extract.cc
+++ wk4/ex27/person/extract.cc
@@ -1,23 +1,27 @@
 #include "person.ih"
 
 void Person::extract(istream &stream) {
-    enum datamember = { NAME, ADDRESS, PHONE, MASS };
+    enum datamember { NAME, ADDRESS, PHONE, MASS };
     datamember member = NAME;
+
     string value;
-    while(getline(stream, value, ',')) 
+    while (getline(stream, value, ',')) 
     {
-        switch(member++)
+        switch (member)
         {
             case NAME:
                 setName(value);
+                member = ADDRESS;
                 break;
 
             case ADDRESS:
                 setAddress(value);
+                member = PHONE;
                 break;
 
             case PHONE:
                 setPhone(value);
+                member = MASS;
                 break;
 
             case MASS:
