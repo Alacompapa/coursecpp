a5002714a1c6a1a4027a84d1aee2024cf358c614
j-boers-13 <j.boers.13@student.rug.nl>
2019-10-04 15:58:38 +0200

    Ex29

--- wk4/ex29/main.cc
+++ wk4/ex29/main.cc
@@ -3,4 +3,11 @@
 int main(int argc, char **argv)
 {
     User test;
+    cout << test.valid() << '\n' <<
+            test.groupId() << '\n' <<
+            test.homeDir() << '\n' <<
+            test.name() << '\n' <<
+            test.realName() << '\n' <<
+            test.shell() << '\n' <<
+            test.userId() << '\n';
 }
--- wk4/ex29/user/user1.cc
+++ wk4/ex29/user/user1.cc
@@ -10,13 +10,4 @@ User::User()
     d_groupId = gid;
     d_homeDir = dir;
     d_userId = uid;
-
-// REMOVE BEFORE HANDING IN
-    cout << "Constructor User::User() called \n";
-
-    cout << d_name << '\n';
-    cout << d_realName << '\n';
-    cout << d_shell << '\n';
-    cout << d_groupId << '\n';
-    cout << d_userId << '\n';
 }
