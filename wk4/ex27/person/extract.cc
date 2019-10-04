7a7d6ece04f241ab893ed2af2df7b42517c3af4c
Bastiaan Boland <bastiaan.boland@gmail.com>
2019-10-04 12:14:15 +0200

    Testing using enums

--- wk4/ex27/person/extract.cc
+++ wk4/ex27/person/extract.cc
@@ -1,10 +1,29 @@
 #include "person.ih"
 
 void Person::extract(istream &stream) {
+    enum datamember = { NAME, ADDRESS, PHONE, MASS };
+    datamember member = NAME;
     string value;
     while(getline(stream, value, ',')) 
     {
-        cout << value << '\n';
+        switch(member++)
+        {
+            case NAME:
+                setName(value);
+                break;
+
+            case ADDRESS:
+                setAddress(value);
+                break;
+
+            case PHONE:
+                setPhone(value);
+                break;
+
+            case MASS:
+                setMass(stoi(value));
+                break;
+        }
 
         if (stream.eof())
             break;
