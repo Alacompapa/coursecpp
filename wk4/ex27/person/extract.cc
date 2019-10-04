72b0985a308142dbca979eaf56f2b2eccc1fb2d6
Bastiaan Boland <bastiaan.boland@gmail.com>
2019-10-04 11:01:56 +0200

    typo

--- wk4/ex27/person/extract.cc
+++ wk4/ex27/person/extract.cc
@@ -2,11 +2,11 @@
 
 void Person::extract(istream &stream) {
     string value;
-    while(getline(istream, value, ',')) 
+    while(getline(stream, value, ',')) 
     {
         cout << value << '\n';
 
-        if (istream.eof())
+        if (stream.eof())
             break;
     }
 }
