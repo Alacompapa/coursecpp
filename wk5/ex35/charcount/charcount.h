2a88c412207fe8e1391fd594c4987c2ffce1fe71
BobbyBobster <bastiaan.boland@gmail.com>
2019-10-06 16:19:26 +0200

    structs need to be defined in class header

--- wk5/ex35/charcount/charcount.h
+++ wk5/ex35/charcount/charcount.h
@@ -3,17 +3,34 @@
 
 #include <iostream>
 
+struct Char
+{
+    char ch;                        // character seen
+    size_t count;                   // frequency of character
+};
+
+struct CharInfo
+{
+    size_t nCharObj;                // size of array
+    struct Char ptr[];              // array of Char objects
+};
+
 enum Actions { APPEND, INSERT, INC };
 
 class CharCount
 {
-                                    // data members
+    struct Char d_Char;
+    struct CharInfo d_CharInfo;
+
     public:
         size_t count(std::istream); // const    and    const?
+                                    // count() should handle 
         struct CharInfo const &info();
 
     private:
-        Actions locate();
+        Actions locate(char ch); // should maybe also return index of where char
+                          //   was found?
+                          //
 };
         
 #endif
