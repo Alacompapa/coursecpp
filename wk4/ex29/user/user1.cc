7fc246ac194362b13fb8b352e4f25f1aaee46a9e
j-boers-13 <j.boers.13@student.rug.nl>
2019-10-09 22:00:11 +0200

    testing 1212

--- wk4/ex29/user/user1.cc
+++ wk4/ex29/user/user1.cc
@@ -2,8 +2,15 @@
 
 User::User()
 {                                           // Get user info from getpwent
-    passwd *pw = getpwent();
 
-    if (pw != 0)
-        auto [d_name, passwd, d_realName, d_shell, d_groupId, d_homeDir, d_userId] = *pw;
+    passwd pw = *getpwent();
+
+    if (pw != NULL){
+        d_name = pw.pw_name;
+        d_realName = pw.pw_gecos;
+        d_shell = pw.pw_shell;
+        d_groupId = pw.pw_gid;
+        d_homeDir = pw.pw_dir;
+        d_userId = pw.pw_uid;
+    }
 }
