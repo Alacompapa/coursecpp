2b3c4400c3fd457c46fec848a3881bf8af621720
BobbyBobster <bastiaan.boland@gmail.com>
2019-10-09 16:09:59 +0200

    backup

--- wk5/ex35/charcount/insert.cc
+++ wk5/ex35/charcount/insert.cc
@@ -8,6 +8,16 @@ Char* CharCount::insert(Char *old, size_t oldsize,
     Char charObj = { ch, 1 };
     cout << index << " INDEX" << '\n';
     cout << charObj.ch << ' ' << charObj.count << " times \n";
+
+    for (size_t idx = 0; idx != index; ++idx)
+        tmp[idx] = old[idx];
+
+    tmp[index] = charObj;
+
+    for (size_t idx = index + 1; idx < newsize; ++idx)
+        tmp[idx] = old[idx- 1]
+
+    /*
     for (size_t idx = 0; idx < newsize; ++idx)
         //tmp[idx] = (idx == index) ? charObj : old[idx];
     {
@@ -16,8 +26,14 @@ Char* CharCount::insert(Char *old, size_t oldsize,
             tmp[idx] = charObj;
         }
         else
+        {
             tmp[idx] = old[idx];
+        }
+
+        cout << tmp[idx].ch;
     }
+    */
+cout << '\n';
 
     delete[] old;
     return tmp;
