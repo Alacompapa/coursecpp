986b96896b61908e6a7d648a985753003c5fe607
Alacompapa <jashiel.work@gmail.com>
2019-10-09 22:09:57 +0200

    fix

--- wk4/ex25/main.cc
+++ wk4/ex25/main.cc
@@ -4,6 +4,6 @@ int main(int argc, char **argv)
 {
     for (auto [year, amount, interest, idx] = argGen(argc, argv); 
             idx != year;
-            ++idx, amount *= (1 + interest/100.0))
+            ++idx, amount *= (1 + interest / 100.0))
         cout << "At year " << idx << ": amount = " << amount << '\n';
 }
