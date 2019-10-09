9ba308a11c3ca3fe28a1d86dc8b038b1b52f92a0
BobbyBobster <bastiaan.boland@gmail.com>
2019-10-09 22:03:59 +0200

    does compile with -> notation

--- wk4/ex29/user/user1.cc
+++ wk4/ex29/user/user1.cc
@@ -3,14 +3,14 @@
 User::User()
 {                                           // Get user info from getpwent
 
-    passwd pw = *getpwent();
+    passwd *pw = getpwent();
 
     if (pw != NULL){
-        d_name = pw.pw_name;
-        d_realName = pw.pw_gecos;
-        d_shell = pw.pw_shell;
-        d_groupId = pw.pw_gid;
-        d_homeDir = pw.pw_dir;
-        d_userId = pw.pw_uid;
+        d_name = pw->pw_name;
+        d_realName = pw->pw_gecos;
+        d_shell = pw->pw_shell;
+        d_groupId = pw->pw_gid;
+        d_homeDir = pw->pw_dir;
+        d_userId = pw->pw_uid;
     }
 }
