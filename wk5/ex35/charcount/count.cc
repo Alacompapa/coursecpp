5266272ff084ccab295a4b6bc3265d7843983dac
j-boers-13 <j.boers.13@student.rug.nl>
2019-10-09 14:58:27 +0200

    gekke shit

--- wk5/ex35/charcount/count.cc
+++ wk5/ex35/charcount/count.cc
@@ -9,24 +9,28 @@ size_t CharCount::count(std::istream& stream)
         ++counter;
 
         auto [ action, index ] = locate(ch);
-        cout << action << " at " << index << '\n';
+        cout << "COUNT " << d_CharInfo.ptr[index].count << '\n';
+
         switch (action)
         { // inserting and appending is kinda the same thing so we can probably use fallthrough
             case INC:
                 // increment ptr[index].count
                 ++d_CharInfo.ptr[index].count;
+                cout << "INC" << '\n';
                 break;
             case APPEND:
                 // append ch at end of `Char ptr[]`
-                ++index;
+                cout << "APPEND" << '\n';
             case INSERT:
                 // insert another Char entry in ptr array
+                cout << "INSERT" << '\n';
                 d_CharInfo.ptr = insert(d_CharInfo.ptr, d_CharInfo.nCharObj, ch, index);
                 ++d_CharInfo.nCharObj;
                 ++d_CharInfo.ptr[index].count;
-                cout << d_CharInfo.nCharObj << '\n' << 
+                cout << d_CharInfo.nCharObj << '\n' <<
                     d_CharInfo.ptr[index].ch << '\n';
                 break;
+
         }
     }
 
--- wk5/ex35/charcount/insert.cc
+++ wk5/ex35/charcount/insert.cc
@@ -1,19 +1,20 @@
 #include "charcount.ih"
 
-Char* CharCount::insert(Char *old, size_t oldsize, 
+Char* CharCount::insert(Char *old, size_t oldsize,
                         unsigned char ch, size_t index)
 {
     size_t newsize = oldsize + 1;
     Char *tmp = new Char[newsize];
-
     Char charObj = { ch, 1 };
-
+    cout << index << " INDEX" << '\n';
     cout << charObj.ch << ' ' << charObj.count << " times \n";
-    for (size_t idx = 0; idx != newsize; ++idx)
+    for (size_t idx = 0; idx < newsize; ++idx)
         //tmp[idx] = (idx == index) ? charObj : old[idx];
     {
         if (idx == index)
+        {
             tmp[idx] = charObj;
+        }
         else
             tmp[idx] = old[idx];
     }
