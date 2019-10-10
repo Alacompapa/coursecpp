fcad79394e457dd16498736c25e228441e84bacc
Bastiaan Boland <bastiaan.boland@gmail.com>
2019-10-10 12:14:25 +0200

    for loops into 1

--- wk5/ex35/charcount/insert.cc
+++ wk5/ex35/charcount/insert.cc
@@ -7,13 +7,15 @@ Char* CharCount::insert(Char *old, size_t oldsize,
     Char *tmp = new Char[newsize];
     Char charObj = { ch, 1 };
 
-    for (size_t idx = 0; idx != index; ++idx)
-        tmp[idx] = old[idx];
-
-    tmp[index] = charObj;
-
-    for (size_t idx = index + 1; idx < newsize; ++idx)
-        tmp[idx] = old[idx - 1];
+    for (size_t idx = 0; idx != newsize; ++idx)
+    {
+        if (idx < index)
+            tmp[idx] = old[idx];
+        else if (idx == index)
+            tmp[idx] = charObj;
+        else if (idx > index)
+            tmp[idx] = old[idx - 1];
+    }
 
     delete[] old;
     return tmp;
