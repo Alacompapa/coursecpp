1829cd661c0e01e9853955d3fb36ed9e04068055
Bastiaan Boland <bastiaan.boland@gmail.com>
2019-10-16 13:23:07 +0200

    moved some lines around nothing major

--- wk5/ex35/charcount/count.cc
+++ wk5/ex35/charcount/count.cc
@@ -3,21 +3,19 @@
 size_t CharCount::count(std::istream& stream)
 {
     unsigned char ch;
-    while(stream >> std::noskipws >> ch)
+    while (stream >> std::noskipws >> ch)
     {
         auto [ action, index ] = locate(ch);
 
         switch (action)
-        { // inserting and appending is kinda the same thing so we can probably use fallthrough
+        { 
             case INC:
-                // increment ptr[index].count
                 ++d_CharInfo.ptr[index].count;
                 break;
-            case APPEND:
-                // append ch at end of `Char ptr[]`
-            case INSERT:
-                // insert another Char entry in ptr array
-                d_CharInfo.ptr = insert(d_CharInfo.ptr, d_CharInfo.nCharObj, ch, index);
+            case APPEND:                // appending ch at end of `Char ptr[]`
+            case INSERT:                // is the same as inserting at end
+                d_CharInfo.ptr = 
+                    insert(d_CharInfo.ptr, d_CharInfo.nCharObj, ch, index);
                 ++d_CharInfo.nCharObj;
                 break;
         }
