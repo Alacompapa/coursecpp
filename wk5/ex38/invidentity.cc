ab933d6abeff34c920e6f943767f432b7e945a54
Bastiaan Boland <bastiaan.boland@gmail.com>
2019-10-16 18:53:38 +0200

    separated funcs into files

---
+++ wk5/ex38/invidentity.cc
@@ -0,0 +1,12 @@
+void inv_identity(int (*rowStart)[10])
+{
+    for (int (*rowPtr)[10] = rowStart;  // get address of start of row
+            rowPtr != rowStart + 10;    
+            ++rowPtr)                   // move rowPtr 10 ints up 
+        for (int *colStart = *rowPtr, *colPtr = *rowPtr; 
+                colPtr != colStart + 10;
+                ++colPtr)               // move colPtr 1 int up
+                                        // 'vertical' distance equal to 
+                                        // 'horizontal' distance means diagonal
+            *colPtr = (rowPtr-rowStart)==(colPtr-colStart) ? 0 : 1;
+}
--- wk5/ex38/main.cc
+++ wk5/ex38/main.cc
@@ -1,18 +1,7 @@
 #include <iostream>
 using namespace std;
 
-void inv_identity(int (*rowStart)[10])
-{
-    for (int (*rowPtr)[10] = rowStart;  // get address of start of row
-            rowPtr != rowStart + 10;    
-            ++rowPtr)                   // move rowPtr 10 ints up 
-        for (int *colStart = *rowPtr, *colPtr = *rowPtr; 
-                colPtr != colStart + 10;
-                ++colPtr)               // move colPtr 1 int up
-                                        // 'vertical' distance equal to 
-                                        // 'horizontal' distance means diagonal
-            *colPtr = (rowPtr-rowStart)==(colPtr-colStart) ? 0 : 1;
-}
+void inv_identity(int (*rowStart)[10]);
 
 int main(int argc, char **argv)
 {
@@ -20,13 +9,4 @@ int main(int argc, char **argv)
     int (*row)[10] = square;                // row is a ptr to 10 ints
 
     inv_identity(row);
-    /* 
-    // To test if matrix is correct, remove before handing in
-    for (size_t idx = 0; idx != 100; ++idx)
-    {
-        if (idx % 10 == 0)
-            cout << '\n';
-        cout << *((*square) + idx);
-    }
-    */
 }
