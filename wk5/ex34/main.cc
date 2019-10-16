ad45e55e8acec2c325188128b95ea7798320fe58
j-boers-13 <j.boers.13@student.rug.nl>
2019-10-16 12:46:24 +0200

    Fixed 34

--- wk5/ex34/main.cc
+++ wk5/ex34/main.cc
@@ -7,20 +7,20 @@ extern char **environ;
 
 int main(int argc, char *argv[])
 {
+    
     char **cpArgv = argv;                       // copy argv and argc
-    int cpArgc = argc;
-
-    for (argc = 0; environ[argc]; ++argc)       // count nr elems environ
-        continue;
-
     argv = environ;                             // switch to where argv
     environ = cpArgv;                           // and environ point
 
+    int envLen;
+    for (envLen = 0; environ[envLen]; ++envLen)       // count nr elems environ
+        continue;
+
     cout << "Environment variables:" << '\n';
-    for (size_t idx = 0; idx != argc; ++idx)    // shows environment vars
-        cout << argv[idx] << '\n';
+    for (char **begin = argv, **end = argv + envLen; begin != end; ++begin)    // shows environment vars
+        cout << *begin << '\n';
 
     cout << "Command line arguments:" << '\n';
-    for (size_t idx = 0; idx != cpArgc; ++idx)  // shows cmd line args
-        cout << environ[idx] << '\n';
+    for (char **begin = environ, **end = environ + argc; begin != end; ++begin)  // shows cmd line args
+        cout << *begin << '\n';
 }
