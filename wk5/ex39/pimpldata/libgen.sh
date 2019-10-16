70a24040555757857520e39d782197eaad747b5b
BobbyBobster <bastiaan.boland@gmail.com>
2019-10-16 19:38:30 +0200

    also added script for removal

--- wk5/ex39/pimpldata/libgen.sh
+++ wk5/ex39/pimpldata/libgen.sh
@@ -1,7 +1,7 @@
-g++ -c main.cc
+g++-8 -Wall -std=c++2a -c main.cc
 cd data                  
-g++ -c display.cc displayimp.cc read.cc readimp.cc data1.cc        
+g++-8 -Wall -std=c++2a -c display.cc displayimp.cc read.cc readimp.cc data1.cc        
 ar rvs libdata.a display.o displayimp.o read.o readimp.o data1.o  
 mv libdata.a ..             
 cd ..
-g++ main.o libdata.a 
+g++-8 main.o libdata.a 
---
+++ wk5/ex39/pimpldata/librm.sh
@@ -0,0 +1,5 @@
+rm *.o
+rm libdata.a
+cd data
+rm *.o
+cd ..
