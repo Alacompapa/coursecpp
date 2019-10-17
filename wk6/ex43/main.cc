ccf9ed01fcf6fd6fca337ce2bfadc6457ee21744
BobbyBobster <bastiaan.boland@gmail.com>
2019-10-17 17:08:38 +0200

    added code from exercisse

---
+++ wk6/ex43/main.cc
@@ -0,0 +1,9 @@
+#include "main.ih"
+
+int main(int argc, char **argv)
+{
+    Strings strings{ cin };
+    for (size_t idx = 0, end = strings.size();
+            idx != end; ++idx)
+        cout << "line " << idx + 1 << ": " << strings.at(idx) << '\n';
+}
