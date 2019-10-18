6ae276699ab8743e6c1df7fb57a4f84c1f228d02
j-boers-13 <j.boers.13@student.rug.nl>
2019-10-18 12:07:17 +0200

    Initial commit for ex44

---
+++ wk6/ex44/strings/add.cc
@@ -0,0 +1,15 @@
+#include "strings.ih"
+
+void Strings::add(string const &next)
+{
+    cout << d_size;
+    string *tmp = enlarge();            // make room for the next string,
+                                        // tmp is the new string *
+
+    tmp[d_size] = next;                 // store next
+
+    delete[] d_str;                     // return old memory
+
+    d_str = tmp;                        // update d_str and d_size
+    ++d_size;
+}
