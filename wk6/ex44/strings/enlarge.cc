75fea57461d95a2c5efb6789812de54eb575fec4
BobbyBobster <bastiaan.boland@gmail.com>
2019-10-22 21:51:23 +0200

    enlarge with for loop

--- wk6/ex44/strings/enlarge.cc
+++ wk6/ex44/strings/enlarge.cc
@@ -6,9 +6,13 @@ string **Strings::enlarge()
     d_capacity <<= 1;
     string **ret = rawPointers(d_capacity);       // room for an extra string
 
-    for (size_t idx = 0; idx != d_size; ++idx)
-    {  // copy existing strings
+    for (string **begin = d_str, **end = d_str + d_size;
+            begin != end; ++begin)
+        *ret = *begin;                            // copy existing strings
+
+/*
+    for (size_t idx = 0; idx != d_size; ++idx)    // copy existing strings
         ret[idx] = d_str[idx];
-    }
+*/
     return ret;
 }
