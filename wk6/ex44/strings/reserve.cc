5927e0a98f23cda1972fc235faf92691b8336cef
BobbyBobster <bastiaan.boland@gmail.com>
2019-10-22 21:57:51 +0200

    reserve better

--- wk6/ex44/strings/reserve.cc
+++ wk6/ex44/strings/reserve.cc
@@ -1,13 +1,13 @@
 #include "strings.ih"
 
-void reserve(size_t newCap)
+void Strings::reserve(size_t newCap)
 {
     if (newCap <= d_capacity)               // do we need to reserve more?
         return;
     
-    string **tmp = rawPointers(new_cap);    // get more raw memory
+    string **tmp = rawPointers(newCap);     // get more raw memory
     for (string **begin = d_str, **end = d_str + d_size; 
-            begin != end, ++begin) 
+            begin != end; ++begin) 
         *tmp = *begin;                      // copy existing pointers
 
     d_str = tmp; 
