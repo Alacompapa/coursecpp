4bc9ce11d61710a6a5548a44fea9c9e37f2b19b5
Bastiaan Boland <bastiaan.boland@gmail.com>
2019-10-25 09:13:08 +0200

    moved comment

--- wk6/ex41/charcount/insert.cc
+++ wk6/ex41/charcount/insert.cc
@@ -6,13 +6,13 @@ void CharCount::insert(size_t index, unsigned char ch)
         d_CharInfo.ptr = enlarge(d_CharInfo.ptr);
 
     if (index < d_CharInfo.nCharObj)
-    {
+    {               // copy all Chars after index to one place to the right
         for (Char *end = d_CharInfo.ptr + d_CharInfo.nCharObj, 
                 *begin = d_CharInfo.ptr + index;
                 begin != end; --end)
             *end = *(end - 1);
     }
-        // copy all Chars after index to one place to the right
+        
 
     d_CharInfo.ptr[index] = Char{ ch, 1 };
     ++d_CharInfo.nCharObj;
