490cec80b737bb0f0f2eff193a8eacda2104fcde
BobbyBobster <bastiaan.boland@gmail.com>
2019-11-05 14:10:28 +0100

    explanation comments layout

--- wk7/ex49/main.cc
+++ wk7/ex49/main.cc
@@ -2,23 +2,23 @@
 
 using namespace std;
 
-// Internally, the insertion operator accesses the output sequence by first constructing a sentry object, then performing the formatting and insertion operations, then setting the internal state flags, s and then destroys the object before returning.
-// By first checking if failbit is set, we can bypass this object entirely, which saves on execution time
+// The insertion operator is a function call, therefore a stack frame will be created and filled with the arguments of the call. Then the ostream class has a member class sentry of which an object is created before (and after) each output operation. This object then checks for the goodbit of the ostream. After the operation the sentry object is destroyed.
+
+// In summary if we don't bypass the insertion operation (by not adding the if statement), a stack frame is created and filled, an object is created, the object calls a function and the object is destroyed.
+// This all takes time. Not much time but when we do a large number of repetitions this adds up.
 
 // Rule of thumb:
 // The insertion operator's failbit is useful for not inserting sequences to an output stream. If the insertion operator has to be called a large amount of times on the same stream, including an if-statement to check for the failbit before using the insertion operator increases performance significantly.
 
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-    if (argc < 2)
+    if (argc < 2)                       // not enough arguments
         return 1;
-    else {
-        std::ostream out(std::cout.rdbuf());
-        out.setstate(ios::failbit);
 
-        for (size_t count = 0; count < stoul(argv[1]); ++count) {
-            if (out.good())
-                out << "Yes";
-        }
-    }
+    ostream out(cout.rdbuf());
+    out.setstate(ios::failbit);
+
+    for (size_t count = 0; count != stoul(argv[1]); ++count) 
+        if (out.good())
+    		out << "Nr. of command line arguments " << argc << '\n';
 }
