23b20a95746d5bdee9a3aba0c21bc7cf26dd8b1a
j-boers-13 <j.boers.13@student.rug.nl>
2019-11-05 14:28:17 +0100

    ex50 explanation

--- wk7/ex50/main.cc
+++ wk7/ex50/main.cc
@@ -12,6 +12,9 @@ int main()
     ofstream out1{ "/tmp/out1" };
     hello(out1);
 
+    // fstream requires flags for writing and reading.
+    // If we don't initialize fstream with ios::out flag, it won't be able to
+    // write by means of insertion operator
     fstream out2{ "/tmp/out2", ios::out };
     hello(out2);
 }
