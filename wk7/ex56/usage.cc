d019ab5a651f54fbbc8ab4d20d8611fc920b1bbf
BobbyBobster <bastiaan.boland@gmail.com>
2019-11-07 11:58:36 +0100

    added usage mesage

--- wk7/ex56/usage.cc
+++ wk7/ex56/usage.cc
@@ -1,8 +1,13 @@
 #include "main.ih"
 
-void usage(char *program)
+void usage(char const *program)
 {
-    // TODO: write usage func
-    // infilepath, outfilepath, [-b]
-    cout << "USAGE \n";
+    cout << 
+    "Usage: " << program << R"R( inputfile outputfile [-b]
+Where:
+  inputfile  - filename of input
+  outputfile - filename of output
+  -b         - option to write binary format to outputfile
+
+Outputfile is overwritten if present, otherwise it is created.)R" << '\n';
 }
