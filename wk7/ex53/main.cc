7969bd86b915af7410ac1a3ca2c6e3e233724494
BobbyBobster <bastiaan.boland@gmail.com>
2019-11-07 12:10:25 +0100

    fixed layout

--- wk7/ex53/main.cc
+++ wk7/ex53/main.cc
@@ -7,10 +7,28 @@ int main()
 {
     double value = 12.04;
 
-    cout << "std display      " << '\'' << setw(15)              << value    << '\'' << '\n'
-         << "left aligned     " << '\'' << setw(15) << left      << value << "'\n"
-         << "right aligned    " << '\'' << setw(15) << right     << value << "'\n"
-         << "3 digits         " << '\'' << setw(15) << setiosflags(ios::fixed) << setprecision(1)    << value   << "'\n"
-         << "4-digit fraction " << '\'' << setw(15) << setprecision(4)                               << fixed << value << "'\n"
-         << "std display      " << '\'' << setw(15) << resetiosflags(ios::fixed)                     << value   << "'\n";
+    cout << "std display      " 
+            << '\'' 
+            << setw(15) << value 
+            << '\'' << '\n'
+         << "left aligned     " 
+            << '\'' 
+            << setw(15) << left << value 
+            << "'\n"
+         << "right aligned    " 
+            << '\'' 
+            << setw(15) << right << value 
+            << "'\n"
+         << "3 digits         " 
+            << '\'' 
+            << setw(15) << setiosflags(ios::fixed) << setprecision(1)<< value   
+            << "'\n"
+         << "4-digit fraction " 
+            << '\'' 
+            << setw(15) << setprecision(4) << fixed << value 
+            << "'\n"
+         << "std display      " 
+            << '\'' 
+            << setw(15) << resetiosflags(ios::fixed) << value 
+            << "'\n";
 }
