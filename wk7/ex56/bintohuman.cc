afe5663ae5a34cf6e9c1cb7c76712f0142f27ed8
BobbyBobster <bastiaan.boland@gmail.com>
2019-11-07 11:34:10 +0100

    Finished ex56 with comments

--- wk7/ex56/bintohuman.cc
+++ wk7/ex56/bintohuman.cc
@@ -3,14 +3,14 @@
 int binToHuman(istream &infile, ostream &outfile)
 {
     char overhead[12];                  
-    infile.read(overhead, 12);     // our files have this header
+    infile.read(overhead, 12);      // our files have this header
     for (size_t idx = 0; idx != sizeof(data::overhead); ++idx)
         if (overhead[idx] != data::overhead[idx])
             return 1;
 
     size_t lettercount = 0;         // header then contains lettercount            
     for (size_t idx = 0, tmpcount = 0; idx != sizeof(size_t); ++idx)
-    {                              // read in little endian order bytes 
+    {                               // read in little endian order bytes 
         tmpcount = infile.get();
         lettercount += (tmpcount << 8*idx);
     }
@@ -18,16 +18,21 @@ int binToHuman(istream &infile, ostream &outfile)
 
     for (size_t idx = 0; idx != lettercount; ++idx) 
     {
-        char letterquad[1];
-        if (!(idx % 4))
-            infile.read(letterquad, 1);
+        char letterquad;            
+        if (!(idx % 4))             // read per 4 letters (1 byte)
+            infile.get(letterquad);
 
+                                    // to read from right to left inside a byte 
+                                    // we need this shift
         size_t shift = 6 - (idx % 4) * 2;
-
+                                    // mask = 11 in binary
+                                    // with shift we 'pick out' a certain
+                                    // letter (bitpair) and isolate it as
+                                    // a two-bit value
         unsigned char mask = 3 << shift;
-        unsigned char base = (*letterquad & mask) >> shift;
+        unsigned char base = (letterquad & mask) >> shift;
 
-        switch (base)
+        switch (base)               // write letter to file
         {
             case data::A:
                 outfile << 'A';
--- wk7/ex56/main.cc
+++ wk7/ex56/main.cc
@@ -4,19 +4,19 @@ int main(int argc, char **argv)
 {
     ifstream infile;
     ofstream outfile;
-    switch (option(argc, argv))
+    switch (option(argc, argv))     // determine arguments
     {
-        case data::HUMANTOBIN:
+        case data::HUMANTOBIN:      // human input -> binary output
             infile.open(argv[1]); 
             outfile.open(argv[2]);
             return humanToBin(infile, outfile);
             break;
-        case data::BINTOHUMAN:
+        case data::BINTOHUMAN:      // binary input -> human output
             infile.open(argv[1]); 
             outfile.open(argv[2]);
             return binToHuman(infile, outfile);
             break;
-        case data::USAGE:
+        case data::USAGE:           // unrecognized arguments
         default:
             usage(argv[0]);
     }
