809b89753731b932917a8ce2b717d44b13641d78
Alacompapa <jashiel.work@gmail.com>
2019-11-13 20:47:28 +0100

    Update swap.cc

--- wk8/ex60/demo/swap.cc
+++ wk8/ex60/demo/swap.cc
@@ -3,7 +3,7 @@
 void Demo::swap(Demo &other)
 {
     char buffer[sizeof(Demo)];
-    memcpy(buffer, &other, sizeof(Demo));
-    memcpy(reinterpret_cast<char *>(&other), this,   sizeof(Demo));
-    memcpy(reinterpret_cast<char *>(this),   buffer, sizeof(Demo));
+    memcpy(buffer, this, sizeof(Demo));
+    memcpy(reinterpret_cast<void *>(this), &other,   sizeof(Demo));
+    memcpy(reinterpret_cast<void *>(&other), buffer, sizeof(Demo));
 }
