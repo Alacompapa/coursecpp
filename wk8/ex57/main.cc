03a996572b3e79986c7de0e558ff9b57cc5f1ab5
Alacompapa <jashiel.work@gmail.com>
2019-11-13 16:29:30 +0100

    added string header

--- wk8/ex57/main.cc
+++ wk8/ex57/main.cc
@@ -3,7 +3,10 @@
 int main(int argc, char **argv)
 {
     Strings strings{ cin };
+
     for (size_t idx = 0, end = strings.size();
+
             idx != end; ++idx)
+
         cout << "line " << idx + 1 << ": " << strings.at(idx) << '\n';
 }
