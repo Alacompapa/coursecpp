bf46bcf34c46c4426733ec7785d4f72b4547f431
Alacompapa <jashiel.work@gmail.com>
2019-11-15 10:55:34 +0100

    Update factory.cc

--- wk8/ex60/factory.cc
+++ wk8/ex60/factory.cc
@@ -1,5 +1,5 @@
 #include "main.ih"
-
+// NOTE(jashiel): we don't have to include factory.cc in our answers so we can just pretend a bit.
 // NOTE(bb): not finished, dont know how to implement factory function
 Demo factory()
 {
