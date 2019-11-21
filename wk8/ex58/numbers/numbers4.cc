a541ded5557e03aa73c16704e8bb9fab4cdaf488
j-boers-13 <j.boers.13@student.rug.nl>
2019-11-21 01:22:40 +0100

    Small fixes

--- wk8/ex58/numbers/numbers4.cc
+++ wk8/ex58/numbers/numbers4.cc
@@ -6,7 +6,5 @@ Numbers::Numbers(size_t count, size_t value) // init count ints to value
     d_count(count)
 {
     for (size_t idx = 0; idx != count; ++idx)
-    {
         d_numbers[idx] = value;
-    }
 }
