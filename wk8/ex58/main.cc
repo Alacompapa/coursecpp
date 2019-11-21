944543b7a62566d6701df263a32590ea5fc81b7e
j-boers-13 <j.boers.13@student.rug.nl>
2019-11-21 01:24:02 +0100

    Rearrange main

--- wk8/ex58/main.cc
+++ wk8/ex58/main.cc
@@ -2,18 +2,22 @@
 
 int main(int argc, char **argv)
 {
+
+
     Numbers numbers { 10, 1 }; // Initialize to 1
-    Numbers numbers2 { 10 }; // Default initialization
-    int *numbers3 = new int[10](); // Initialize to 0
 
     numbers.at(3) = 4;
     numbers.write(cout);
     cout << numbers.length() << '\n';
 
+    Numbers numbers2 { 10 }; // Default initialization
+
     numbers2.at(3) = 4;
     numbers2.write(cout); // Will output 0004000000
     cout << numbers2.length() << '\n';
 
+    int *numbers3 = new int[10](); // Initialize to 0
+    
     for (size_t idx = 0; idx != 10; ++idx) {    // Will output 0000000000
         cout << numbers3[idx];
     }
